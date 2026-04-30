ae680d58cad5eda18e39775998da1c21c84b0947
Olesia Yankiv <olesia.yankiv.shi.2023@lpnu.ua>
2026-04-30 09:58:57 +0200

    Changes for the Lab 4 (Terraform IaC)

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "resource_group_location" {
+  type        = string
+  default     = "swedencentral"
+}
+variable "resource_group_name_prefix" {
+  type        = string
+  default     = "rg-analytics-v2"
+}
